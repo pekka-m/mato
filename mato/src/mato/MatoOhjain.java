448b1816134715cfc5f3ec2b43e468ea6b775e0e
pekka-m <propeke@gmail.com>
2014-03-14 10:29:51 +0200

    mato begins

---
+++ mato/src/mato/MatoOhjain.java
@@ -0,0 +1,23 @@
+package mato;
+
+/* @author H3173 */
+public class MatoOhjain {
+
+    private Matopeli malli;
+    private MatoGUI näkymä;
+
+    public MatoOhjain(Matopeli malli, MatoGUI näkymä) {
+        this.malli = malli;
+        this.näkymä = näkymä;
+    }
+
+    public static void main(String[] args) {
+        Matopeli malli = new Matopeli();
+        MatoGUI näkymä = new MatoGUI();
+        MatoOhjain ohjain = new MatoOhjain(malli, näkymä);
+        näkymä.rekisteröiOhjain(ohjain);
+    }
+}
+
+
+
