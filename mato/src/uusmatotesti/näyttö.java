916a37f2148f12e5c9c6d9412a173a0d249b0119
pekka-m <propeke@gmail.com>
2014-03-27 16:12:54 +0200

    eksponentiaalista kasvua

--- mato/src/uusmatotesti/näyttö.java
+++ mato/src/uusmatotesti/näyttö.java
@@ -6,10 +6,11 @@ import java.util.Random;
 import javax.swing.*;
 
 /**
+ * 
  *
  * @author H3173
  */
-public class näyttö extends JPanel implements Runnable  {
+public class näyttö extends JPanel implements Runnable{
 
     private final int borderx = 690;
     private final int bordery = 530;
@@ -23,13 +24,17 @@ public class näyttö extends JPanel implements Runnable  {
     private boolean seinä;
     private final JLabel ennätys;
     private int pisteet = 0;
-    private int madonpituus = 3;
+    private int madonpituus = 10;
     // taulukoihin tallennetaan madon osien x ja y koordinaatit
     private final ArrayList<Integer> madonosatx = new ArrayList<>();
     private final ArrayList<Integer> madonosaty = new ArrayList<>();
     private JLabel häviöteksti;
     private Thread thread;
     private Random arpoja = new Random();
+    private int vikay;
+    private int tokavikay;
+    private int vikax;
+    private int tokavikax;
 
     public näyttö() {
         setPreferredSize(new Dimension(borderx, bordery));
@@ -38,14 +43,17 @@ public class näyttö extends JPanel implements Runnable  {
         y = 240;
 
         // pelin alussa alustetaan madonosataulukon kaikki paikat
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < madonpituus; i++) {
             madonosatx.add(320);
             madonosaty.add(240);
         }
+        vikay = madonosaty.get(madonpituus - 1);
+        tokavikay = madonosaty.get(madonpituus - 2);
+        vikax = madonosatx.get(madonpituus - 1);
+        tokavikax = madonosatx.get(madonpituus - 2);
         SuunnanArpoja();
 
         // arvotaan ensimmäinen safka
-
         arpoja();
         ennätys = new JLabel();
 
@@ -90,7 +98,7 @@ public class näyttö extends JPanel implements Runnable  {
         }
         if (seinä == false) {
             // näytetään madon x ja y koordinaatit
-            
+
             // lisätään koordinaatit ensimmäiseen paikkaan, muut työntyy eteenpäin
             this.madonosatx.add(0, x);
             if (madonosatx.size() == madonpituus) {
@@ -100,6 +108,10 @@ public class näyttö extends JPanel implements Runnable  {
             if (madonosaty.size() == madonpituus) {
                 madonosaty.remove(madonpituus);
             }
+            vikay = madonosaty.get(madonpituus - 1);
+            tokavikay = madonosaty.get(madonpituus - 2);
+            vikax = madonosatx.get(madonpituus - 1);
+            tokavikax = madonosatx.get(madonpituus - 2);
             this.dx = dx;
             this.dy = dy;
             // liikuttaa matoa
@@ -109,19 +121,19 @@ public class näyttö extends JPanel implements Runnable  {
             System.out.print("x: " + äksä() + " ");
             System.out.print("y: " + yyyy() + "\n");
             System.out.println(madonpituus);
-            
+
             for (int i = 0; i < madonpituus; i++) {
-                 
-            System.out.println(madonosatx.get(i));
-            System.out.println(madonosaty.get(i));
-                
+
+                System.out.println(madonosatx.get(i));
+                System.out.println(madonosaty.get(i));
+
                 if ((madonosatx.get(i) == x) && (madonosaty.get(i) == y)) {
                     System.out.println("Hävisit pelin!");
-                    seinä =true;
+                    seinä = true;
                     hävisitTeksti();
                 }
             }
-            
+
             //ollaanko törmätty safkaan
             this.törmäys = (x == safkax) & (y == safkay);
         }
@@ -133,40 +145,72 @@ public class näyttö extends JPanel implements Runnable  {
             siirrä(dx, dy);
             repaint();
             try {
-                Thread.sleep(500);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
             }
         }
 
     }
-
     @Override
     public void paintComponent(Graphics g) {
+        // mahdollisesti affinetransform juttua käytetään kuvan kääntämiseen
         super.paintComponent(g);
-        g.setColor(Color.white);
-        g.fillRect(x, y, 20, 20);
-        g.fillRect(0, 0, 700, 20); // yläseinä
+
+        // madon pää
+        if (dx == 20 && dy == 0) {
+            Image madonpää = Toolkit.getDefaultToolkit().getImage("madonpaa-oikea.png");
+            g.drawImage(madonpää, x, y, null);
+        } else if (dx == 0 && dy == 20) {
+            Image madonpää = Toolkit.getDefaultToolkit().getImage("madonpaa-alas.png");
+            g.drawImage(madonpää, x, y, null);
+        } else if (dx == -20 && dy == 0) {
+            Image madonpää = Toolkit.getDefaultToolkit().getImage("madonpaa-vasen.png");
+            g.drawImage(madonpää, x, y, null);
+        } else if (dx == 0 && dy == -20) {
+            Image madonpää = Toolkit.getDefaultToolkit().getImage("madonpaa-ylos.png");
+            g.drawImage(madonpää, x, y, null);
+        }
+        
+        // yläseinä jne
+        g.setColor(Color.darkGray);
+        g.fillRect(0, 0, 700, 20);
         g.fillRect(0, 0, 20, 540);
         g.fillRect(680, 0, 20, 540);
         g.fillRect(0, 520, 700, 20);
 
-
-
-        if (this.x == 0) {
-            g.fillRect(0, 0, 40, 40);
+        // piirretään madon keskiosa, pituus muuttujasta madonpituus, koordinaatit taulukoista
+        for (int i = 0; i < madonpituus-1; i++) {
+            Image madonruho = Toolkit.getDefaultToolkit().getImage("madonruho.png");
+            g.drawImage(madonruho, madonosatx.get(i), madonosaty.get(i), null);
         }
 
-        // piirretään madon osat, pituus muuttujasta madonpituus, koordinaatit taulukoista
-        for (int i = 0; i < madonpituus; i++) {
-            g.fillRect(madonosatx.get(i), madonosaty.get(i), 20, 20);
+        // madon jalat
+        if (vikay == tokavikay) {
+            if (vikax < tokavikax) {
+                Image madonjalat = Toolkit.getDefaultToolkit().getImage("madonjalat-oikea.png");
+                g.drawImage(madonjalat, madonosatx.get(madonpituus-1), madonosaty.get(madonpituus-1), null);
+            }
+            else {
+                Image madonjalat = Toolkit.getDefaultToolkit().getImage("madonjalat-vasen.png");
+                g.drawImage(madonjalat, madonosatx.get(madonpituus-1), madonosaty.get(madonpituus-1), null);
+            }
         }
-
-        g.setColor(Color.red);
+        else {
+            if (vikay < tokavikay) {
+                Image madonjalat = Toolkit.getDefaultToolkit().getImage("madonjalat-alas.png");
+                g.drawImage(madonjalat, madonosatx.get(madonpituus-1), madonosaty.get(madonpituus-1), null);
+            }
+            else {
+                Image madonjalat = Toolkit.getDefaultToolkit().getImage("madonjalat-ylos.png");
+                g.drawImage(madonjalat, madonosatx.get(madonpituus-1), madonosaty.get(madonpituus-1), null);
+            }
+        }
         this.repaint();
-
+        Image apple = Toolkit.getDefaultToolkit().getImage("apple.png");
         if (törmäys) {
             arpoja();
-            g.fillRect(safkax, safkay, 20, 20);
+           // g.fillRect(safkax, safkay, 20, 20);  
+            g.drawImage(apple, safkax, safkay, null);
             this.pisteet += 1;
             ennätys.setText("pisteet: " + pisteet);
             törmäys = false;
@@ -174,14 +218,14 @@ public class näyttö extends JPanel implements Runnable  {
             // aina safkatessa madon pituutta lisätään yhdellä
             madonpituus++;
         } else {
-            g.fillRect(safkax, safkay, 20, 20);
+            g.drawImage(apple, safkax, safkay, null);
         }
     }
 
     private void arpoja() {
 
-        this.safkax = arpoja.nextInt(31) * 20;
-        this.safkay = arpoja.nextInt(23) * 20;
+        this.safkax = arpoja.nextInt(31) * 20 + 20;
+        this.safkay = arpoja.nextInt(23) * 20 + 20;
     }
 
     private void SuunnanArpoja() {
